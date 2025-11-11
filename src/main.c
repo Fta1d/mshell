c5caa6413afaade49b57bd00a705ec29cf796d97
Fta1d <li4nomark228@gmail.com>
2025-11-11 12:35:59 +0200

    Added basic structure.

---
+++ src/main.c
@@ -0,0 +1,12 @@
+
+
+int main(char **argv, int argc) {
+    // TODO: Load configuration files
+
+    // Main shell loop
+    mshell_loop();
+
+    // TODO: Execute termination routines
+
+    return 0;
+}
